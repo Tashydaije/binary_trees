62eb10d3e6156c4e47364b5fd487edde2f12915b
Salome-N <njengasalome87@gmail.com>
2024-02-01 16:12:39 -0800

    finds the sibling of a node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,7 +10,7 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL || node-parent == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 	if (node->parent->left == node)
 		return (node->parent->right);
